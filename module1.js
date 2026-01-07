fa3a73c94c7f70b6891435b09e0bde33e6a0f7e0
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2026-01-08 04:41:46 +0530

    Modules

---
+++ module1.js
@@ -0,0 +1,19 @@
+function add(a,b){
+     let sum=a+b
+    console.log("This is the answer of addition",sum);
+}
+
+// add(2,3)
+
+function sub(p,q){
+    let subtract=p-q
+    console.log("This is the answer of subtraction",subtract);
+}
+
+// sub(10,2)
+
+module.exports={
+    add,
+    sub
+}
+
---
+++ module2.js
@@ -0,0 +1,10 @@
+// const total= require("./module1")
+
+// console.log("Export Answer",total);
+
+
+const total = require("./module1");
+
+total.add(2, 3);
+total.sub(10, 2);
+// console.log(total)
