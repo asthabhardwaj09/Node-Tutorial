fe3f9f552f8455addfc46405a3b32b05afa2495d
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2026-01-21 23:53:38 +0530

    create server

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,25 @@
-// const { log } = require("console");
 const http = require("http");
+const fs = require('fs')
 
-const myServer= http.createServer((req,res)=>{
+const myServer = http.createServer((req, res) => {
     // console.log(req.headers)
-    console.log(req)
-    res.end("Hello From Server Again")
+    const log = `${Date.now()}:${req.url}:New Req Recieved\n`
+    fs.appendFile('log.txt', log, (err, data) => {
+        switch (req.url) {
+            case '/':
+                res.end("HomePage")
+                break
+            case '/About':
+                res.end("Welcome to about page")
+                break
+            case '/Contact':
+                res.end("Welcome to contact page")
+            default:
+                res.end('404 error')
+        }
+        // res.end("Hello From Astha Server")
+    })
+
 })
 
-myServer.listen(8000,()=> console.log("server started"));
+myServer.listen(8000, () => console.log("server started"));
