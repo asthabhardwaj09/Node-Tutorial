73e742502b040e9b46a7b4fc28ab9483d669e895
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2026-01-22 15:48:43 +0530

    handeling url

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,15 @@
 const http = require("http");
 const fs = require('fs')
+const url = require('url')
 
 const myServer = http.createServer((req, res) => {
+    if (req.url==='/favicon.ico') return res.end;
     // console.log(req.headers)
     const log = `${Date.now()}:${req.url}:New Req Recieved\n`
+    const myUrl= url.parse(req.url,true)
+    console.log(myUrl);
     fs.appendFile('log.txt', log, (err, data) => {
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case '/':
                 res.end("HomePage")
                 break
@@ -13,7 +17,9 @@ const myServer = http.createServer((req, res) => {
                 res.end("Welcome to about page")
                 break
             case '/Contact':
-                res.end("Welcome to contact page")
+                const qp=myUrl.query.search
+                res.end(`${qp} it is a dog name german saford`)
+                break
             default:
                 res.end('404 error')
         }
