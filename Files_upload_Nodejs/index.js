331e2904a8ce5c53a2786a6c75a76227b6359d47
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2026-04-13 23:51:33 +0530

    model schema design

---
+++ Files_upload_Nodejs/index.js
@@ -0,0 +1,37 @@
+const express= require("express")
+const path =require("path")
+const multer  = require('multer')
+
+const app= express()
+const PORT=7000;
+
+// const upload = multer({ dest: 'uploads/' })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null,"./uploads")
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+
+const upload=multer({storage})
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({extended:false}));
+
+app.get("/",(req,res)=>{
+    return res.render("homepage")
+})
+
+app.post('/upload',upload.single('profileimage'),(req,res)=>{
+    console.log(req.body)
+    console.log(req.file)
+    return res.redirect('/');
+    
+
+})
+
+app.listen(PORT,()=> console.log(`Server started at ${PORT}`));
